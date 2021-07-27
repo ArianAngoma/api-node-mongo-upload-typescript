a93ab757017f5793b928a83f9684244c135e50a8
unknown <arian.angoma@tecsup.edu.pe>
2021-07-27 17:02:17 -0500

    Debug project

--- src/models/server.ts
+++ src/models/server.ts
@@ -1,15 +1,39 @@
 import express, {Application} from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 
 class Server {
     private app: Application;
     private readonly port: string;
-    private path = {
-
-    }
+    private path = {}
 
     constructor() {
         this.app = express();
         this.port = process.env.PORT || '4000';
+
+        // Middlewares
+        this.middlewares();
+
+        // Definir las rutas
+        this.routes();
+    }
+
+    middlewares(): void {
+        // Morgan
+        this.app.use(morgan('dev'));
+
+        // CORS
+        this.app.use(cors());
+
+        // Lectura del body
+        this.app.use(express.json());
+
+        // Carpeta public
+        this.app.use(express.static('public'));
+    }
+
+    routes(): void {
+        //this.app.use(this.path.users, userRoutes);
     }
 
     listen(): void {
