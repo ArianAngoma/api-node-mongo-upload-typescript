d4c127947ae9134945d01069d62aed6f8c70d0f1
unknown <arian.angoma@tecsup.edu.pe>
2021-07-29 13:55:27 -0500

    Create new Img

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,11 +1,18 @@
 import {Request, Response} from "express";
-import {upload} from '../helpers/multer';
+import Image from '../models/images';
 
 export const getImg = async (req: Request, res: Response): Promise<void> => {
     res.send('Hola');
 }
 
 export const createImg = async (req: Request, res: Response): Promise<void> => {
-    console.log(req.file);
-    res.send('Hola');
+    const {title, description} = req.body;
+    const newImg = {
+        title,
+        description,
+        img: req.file!.path
+    };
+    const img = new Image(newImg);
+    await img.save();
+    res.json({img});
 }
